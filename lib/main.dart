c6f5c00fe08ca922dcbe4be7842889ddf9447cb9
sodipto <badhansaha444@gmail.com>
2021-02-16 01:08:59 +0600

    done login page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Open Sans'
       ),
-      home: LoginPage() //HomePage()
+      home: AuthPage() //HomePage()
     );
   }
 }
