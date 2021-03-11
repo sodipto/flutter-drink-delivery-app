1986fd9f9d8d30baf483adf711e2155e8b5083a1
sodipto <badhansaha444@gmail.com>
2021-03-11 22:29:41 +0600

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Open Sans'
       ),
-      home: ProductReview() //AuthPage()
+      home: ProductReview()
     );
   }
 }
